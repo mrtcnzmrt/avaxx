d06e62744cf6a95620393d89a04ba005ed867ff1
hasankoroglu <mertcanzumrut@hotmail.com>
2022-05-17 23:20:06 +0300

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,28 @@
+require("@nomiclabs/hardhat-waffle");
+
+const PRIVATE_KEY = "228026fa8e40a5bf4e85d5bbe540890e7e733ea6e1dca6a9aa0325d4e438472f"; // Your wallets private key
+
+module.exports = {
+  solidity: {
+    version: "0.7.0",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+
+  networks: {
+    fuji: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      chainId: 43113,
+      accounts: [PRIVATE_KEY],
+    },
+    mainnet: {
+      url: "https://api.avax.network/ext/bc/C/rpc",
+      chainId: 43114,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+};
